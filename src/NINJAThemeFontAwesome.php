11234620031e2f04671b465ae033e5db394bb9b8
“NinjaArmy” <jm@bitsandlikes.de>
2023-07-14 00:52:03 +0200

    first commit

---
+++ src/NINJAThemeFontAwesome.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace NINJAThemeFontAwesome;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class NINJAThemeFontAwesome extends Plugin implements ThemeInterface
+{
+}
